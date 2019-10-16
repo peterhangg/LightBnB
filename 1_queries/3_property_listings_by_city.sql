b3ad904e997f63d18ff577d027380b5786676c82
peterhangg <peterhanggg@gmail.com>
2019-10-16 16:05:22 +0000

    edit queries 3 & 6

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -2,6 +2,7 @@ SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN  property_reviews
 ON properties.id = property_id
+WHERE city LIKE '%ancouver%'
 GROUP BY properties.id
 HAVING AVG(property_reviews.rating) >= 4
 ORDER BY cost_per_night
